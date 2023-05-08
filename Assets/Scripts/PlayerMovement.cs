a0f8f153617bec5a7e91c7b1fecac763b594e26f
Tdiddy020105 <thomas.dielesen.gg@gmail.com>
2023-05-08 22:03:19 +0200

    Added map and initial animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public Animator animator;
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", Mathf.Abs(verticalInput));
 
         Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
 
