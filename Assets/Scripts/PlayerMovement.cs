e28b47167489956fc5a744b7169e9756392b947e
ThomasDielesen <thomas.dielesen.gg@gmail.com>
2023-05-09 13:08:50 +0200

    Added npc, fixed animations so that player can walk everyway

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Animator animator;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        /*float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         animator.SetFloat("Speed", Mathf.Abs(verticalInput));
 
         Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
 
-        transform.position += movement;
+        transform.position += movement;*/
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
 
